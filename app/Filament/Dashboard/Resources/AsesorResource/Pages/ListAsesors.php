b348b11e2e1376f3678311778997abfd776299e6
TaniaCardoza <taniacardoza581@gmail.com>
2025-05-20 12:32:31 -0500

    Modulo asesores, resources

---
+++ app/Filament/Dashboard/Resources/AsesorResource/Pages/ListAsesors.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Filament\Dashboard\Resources\AsesorResource\Pages;
+use App\Filament\Dashboard\Resources\AsesorResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+class ListAsesors extends ListRecords
+{
+    protected static string $resource = AsesorResource::class;
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
+
