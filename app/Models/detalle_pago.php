65909e8fb8bd7fd0fc9a4be268a4eaa6033d7bd0
TaniaCardoza <taniacardoza581@gmail.com>
2025-05-25 18:45:25 -0500

    detalle_pago

---
+++ app/Models/detalle_pago.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Pago;
+use App\Models\PrestamoIndividual;
+
+class detalle_pago extends Model
+{
+      use HasFactory;
+
+    protected $table = 'detalles_pago';
+
+    protected $fillable = [
+        'pago_id',
+        'prestamo_individual_id',
+        'monto_pagado',
+        'estado_pago_individual',
+    ];
+
+    /**
+     * Relación con el modelo Pago
+     */
+    public function pago()
+    {
+        return $this->belongsTo(Pago::class);
+    }
+
+    /**
+     * Relación con el modelo PrestamoIndividual
+     */
+    public function prestamoIndividual()
+    {
+        return $this->belongsTo(prestamo_individual::class);
+    }
+}
