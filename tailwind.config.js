518e283bba785f19c2bccbee611665d8d664eea2
Bryan Andree Velezmoro Romero <andreevelez2020@gmail.com>
2025-07-05 18:13:31 -0500

    funciona sin estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,9 @@ import preset from './vendor/filament/filament/tailwind.config.preset'
 
 export default {
     presets: [preset],
-    corePlugins: {
-        textColor: true,
-    },
     content: [
-        './app/Filament/Dashboard//*.php',
-        './resources/views/filament/dashboard//*.blade.php',
-        './resources/css/filament/dashboard//*.css', // ✅ <- AGREGAR ESTO
-        './vendor/filament//*.blade.php',
+        './app/Filament/Dashboard/**/*.php',
+        './resources/views/filament/dashboard/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
     ],
-    safelist: [
-        'text-white',
-    ],
-    theme: {
-        extend: {
-            colors: {
-                white: '#ffffff',
-            },
-        }
-    }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/build/', // ✅ Esto es crucial para producción
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/css/filament/dashboard/theme.css'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tailwindcss(),
     ],
 });
