9808e66ead369baa8e2d82a5a31f1cd91eabf77a
Bryan Andree Velezmoro Romero <andreevelez2020@gmail.com>
2025-07-05 17:38:37 -0500

    final funcional

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/build/', // ✅ Esto es crucial para producción
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/css/filament/dashboard/theme.css'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
-],
+    ],
 });
