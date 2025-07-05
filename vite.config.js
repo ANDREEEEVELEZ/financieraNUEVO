e2a77745680c8ea88afb96319b1a0992a3acc84c
Bryan Andree Velezmoro Romero <andreevelez2020@gmail.com>
2025-07-04 23:22:38 -0500

    final

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
+    base: '/build/', // ✅ Esto es crucial para producción
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
-],
+    ],
 });
-
-
-
