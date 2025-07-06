65cb7b07738e3c4726803cb0613b81fd521af032
Bryan Andree Velezmoro Romero <andreevelez2020@gmail.com>
2025-07-05 19:04:44 -0500

    final

--- resources/css/filament/dashboard/tailwind.config.js
+++ resources/css/filament/dashboard/tailwind.config.js
@@ -1,10 +1,24 @@
-import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
+import preset from './vendor/filament/filament/tailwind.config.preset'
 
 export default {
     presets: [preset],
+    corePlugins: {
+        textColor: true,
+    },
     content: [
-        './app/Filament/Dashboard/**/*.php',
-        './resources/views/filament/dashboard/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
+        './app/Filament/Dashboard//*.php',
+        './resources/views/filament/dashboard//*.blade.php',
+        './resources/css/filament/dashboard//*.css',
+        './vendor/filament//*.blade.php',
     ],
+    safelist: [
+        'text-white',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                white: '#ffffff',
+            },
+        }
+    }
 }
