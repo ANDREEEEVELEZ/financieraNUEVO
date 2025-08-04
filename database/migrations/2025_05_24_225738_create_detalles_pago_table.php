bf4558ecbb76cd7d935a86294c76d1715cd7723b
Bryan Andree Velezmoro Romero <andreevelez2020@gmail.com>
2025-08-04 13:26:13 -0500

    Formatear el código en la migración de 'detalles_pago' para mejorar la legibilidad

--- database/migrations/2025_05_24_225738_create_detalles_pago_table.php
+++ database/migrations/2025_05_24_225738_create_detalles_pago_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('detalles_pago', function (Blueprint $table) {
             $table->id();
             $table->foreignId('pago_id')->constrained('pagos')->onDelete('cascade');
-             $table->foreignId('prestamo_individual_id')->constrained('prestamo_individual')->onDelete('cascade');
-             $table->decimal('monto_pagado', 10, 2)->nullable();
-             $table->enum('estado_pago_individual', ['Pagada', 'Parcial', 'Mora']);
+            $table->foreignId('prestamo_individual_id')->constrained('prestamo_individual')->onDelete('cascade');
+            $table->decimal('monto_pagado', 10, 2)->nullable();
+            $table->enum('estado_pago_individual', ['Pagada', 'Parcial', 'Mora']);
             $table->timestamps();
         });
     }
