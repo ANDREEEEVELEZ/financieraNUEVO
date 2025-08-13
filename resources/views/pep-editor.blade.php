00573f010a2289fdcb4f8d672c851b3fcd3885a7
Bryan Andree Velezmoro Romero <andreevelez2020@gmail.com>
2025-08-13 14:09:44 -0500

    Eliminacion archiovos PEP

--- resources/views/pep-editor.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Editor de Declaración Jurada PEP')
-
-@section('content')
-    @livewire('pep-editor', ['cliente' => $cliente])
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -42,11 +42,6 @@ Route::middleware([
 ])->group(function () {
     Route::get('/pagos/exportar/pdf', [PagoPdfController::class, 'exportar'])->name('pagos.exportar.pdf');
     Route::get('/moras/exportar-pdf', [MoraPdfController::class, 'exportar'])->name('moras.exportar.pdf');
-    
-    // Ruta para el editor de declaración jurada PEP
-    Route::get('/pep-editor/{cliente}', [App\Http\Controllers\PepEditorController::class, 'show'])
-        ->name('pep.editor')
-        ->middleware('role:super_admin|Jefe de operaciones|Jefe de creditos');
 });
 
 // Ruta para cerrar sesión (opcional si no usas el logout de Filament)
