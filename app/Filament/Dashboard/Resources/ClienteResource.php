4ad26165303c9efbfc9183dbb3932db1cab3d0df
Bryan Andree Velezmoro Romero <andreevelez2020@gmail.com>
2025-08-14 14:09:49 -0500

    PEP

--- app/Filament/Dashboard/Resources/ClienteResource.php
+++ app/Filament/Dashboard/Resources/ClienteResource.php
@@ -361,13 +361,6 @@ class ClienteResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make()->icon('heroicon-o-pencil-square'),
-                Tables\Actions\Action::make('declaracion_jurada')
-                    ->label('Declaración Jurada')
-                    ->icon('heroicon-o-document-text')
-                    ->color('info')
-                    ->url(fn (Cliente $record): string => route('declaracion-jurada', ['cliente_id' => $record->id]))
-                    ->openUrlInNewTab()
-                    ->tooltip('Generar Declaración Jurada SBS (PDF)'),
                 Tables\Actions\Action::make('trasladar_cliente')
                     ->label('Trasladar Cliente')
                     ->icon('heroicon-o-arrow-right-circle')
